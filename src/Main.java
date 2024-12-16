610c0e60faa473dbbbf005c09d1137fe454cd296
rodrigojg35 <rojg780@gmail.com>
2024-12-16 02:46:11 -0600

    Commit inicial

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("****************************************");
+        System.out.println("Sea bienvenido al conversor de monedas:");
+        System.out.println();
+        System.out.println("Las opciones disponibles son:");
+        System.out.println("1) Dólar =>> Peso argentino");
+        System.out.println("2) Peso argentino =>> Dólar");
+        System.out.println("3) Dólar =>> Peso argentino");
+        System.out.println("4) Real brasileño =>> Dólar");
+        System.out.println("5) Dólar =>> Peso argentino");
+        System.out.println("6) Peso colombiano =>> Dólar");
+        System.out.println("7) Salir");
+        System.out.println("Elija una opción válida: ");
+        System.out.println("****************************************");
+    }
+}
